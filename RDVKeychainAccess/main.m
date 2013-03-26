bc9c843eacfb0e6ac3ff67035ed46430bb4bbd57
Robert Dimitrov <robert_dimitrov@me.com>
2013-03-26 21:31:12 +0200

    Initial Commit

---
+++ RDVKeychainAccess/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RDVKeychainAccess
+//
+//  Created by Robert Dimitrov on 3/26/13.
+//  Copyright (c) 2013 Robert Dimitrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RDVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RDVAppDelegate class]));
+    }
+}
